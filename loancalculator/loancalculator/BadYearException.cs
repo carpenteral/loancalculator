0111b151e1a0da5af62120b1a1f5ac43bac4ffe9
Adam Carpenter <carpenteral91@gmail.com>
2019-05-14 16:02:34 +0100

    fitnesse changes

---
+++ loancalculator/loancalculator/BadYearException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitnesse_demo
+{
+    public class BadYearException : Exception
+    {
+        public BadYearException(string msg) : base(msg)
+        {
+        }
+    }
+}
---
+++ loancalculator/loancalculator/UKRegNosHandler.cs
@@ -0,0 +1,45 @@
+﻿using fitnesse_demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_demo
+{
+    public class UKRegNosHandler
+    {
+        public string value = "";
+
+
+        public string result()
+        {
+            return nextRegCode(value);
+        }
+      
+
+        public string nextRegCode(String currentValue)
+        {
+            var result = "";
+            int regNo = Int32.Parse(currentValue);
+            if (regNo > 50)
+            {
+                regNo -= 49;
+                result = regNo.ToString("D2").Substring(0, 2);
+            }
+            else
+            {
+                if (regNo == 50)
+                    throw new BadYearException("2050 is an invalid year");
+                else
+                {
+                    regNo += 50;
+                    result = regNo.ToString("D2");
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
