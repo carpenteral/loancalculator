d9c7cb1498fa5d0fa5709d5402c539a5dfa58e01
Adam Carpenter <carpenteral91@gmail.com>
2019-05-14 15:10:49 +0100

    first commit

---
+++ loancalculator/loancalculator/LoanCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitnesse_demo
+{
+    public  class LoanCalculator
+    {
+        private double itsLoanAmount;
+        private double itsRate;
+
+        public  void    setLoanAmount( double amt )
+        {
+            itsLoanAmount = amt;
+        }
+
+        public  void    setFixedRate( double rate )
+        {
+            itsRate = rate;
+        }
+
+        void    execute()
+        {
+
+        }
+
+        public  double  repaymentAmount()
+        {
+            LoanCalculatorImpl lc = new LoanCalculatorImpl(itsLoanAmount, itsRate);
+            return lc.calculateRepaymentAmount();
+        }
+    }
+}
---
+++ loancalculator/loancalculator/LoanCalculatorImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitnesse_demo
+{
+    public  class LoanCalculatorImpl
+    {
+        private double itsLoanAmount;
+        private double itsRate;
+
+        public  LoanCalculatorImpl( double amt, double rate )
+        {
+            itsLoanAmount = amt;
+            itsRate = rate;
+        }
+
+        public  double  calculateRepaymentAmount()
+        {
+            return itsLoanAmount * (1 + itsRate / 100);
+        }
+    }
+}
